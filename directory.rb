e3c2b14ec8be4b7bb7900ed884ea7aa998645d1e
Léa R <lea.rosamond@gmail.com>
2017-11-10 16:21:42 +0000

    Step 8. Ex-2

--- directory.rb
+++ directory.rb
@@ -20,19 +20,32 @@ def input_students
 #return the array of students
 students
 end
+
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1} - #{student[:name]} (#{student[:cohort]} cohort)"
-  end
+  puts "What letter do you want the names of students to start with?"
+  answer = gets.chomp.downcase
+
+    students.each_with_index do |student, index|
+
+      if student[:name].downcase.start_with?(answer)
+        puts "#{index + 1} - #{student[:name]} (#{student[:cohort]} cohort)"
+      end
+
+    end
+
+
 end
 
+
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "However, Overall, we have #{students.count} great students"
+
 end
 
 
@@ -40,4 +53,5 @@ students = input_students
 
 print_header
 print(students)
+
 print_footer(students)
