8e9ab91fc93030bf8cbeeee3cbc80db9b567256b
Léa R <lea.rosamond@gmail.com>
2017-11-11 14:21:38 +0000

    Step 8. Ex 7

--- directory.rb
+++ directory.rb
@@ -6,27 +6,80 @@ def input_students
   students = []
 
   #get the first name
-  name = gets.chomp
-  puts "Enter their hobbies"
-  #while the name is not empty repeat this code
-  hobbies = gets.chomp
-  puts "Enter their country of birth"
+  name = gets.chomp.to_s
 
-  country = gets.chomp
-  puts "Enter their height"
-  height = gets.chomp
+  puts "Please enter their cohort"
+  cohort = gets.chomp.to_s
+  # puts "Enter their hobbies"
+  # #while the name is not empty repeat this code
+  # hobbies = gets.chomp
+  # puts "Enter their country of birth"
+  #
+  # country = gets.chomp
+  # puts "Enter their height"
+  # height = gets.chomp
+
+
+#for it to be applicable to the first student.
+  if name.nil?
+    name = "Default"
+  end
+  if cohort.nil?
+     cohort = "november"
+  end
+
+    puts "Enter 1 to change the name, 2 to change the cohort or 3 to continue"
+      answer = gets.chomp.to_i
+      if  answer == 2
+        puts "Type the cohort again"
+        cohort = gets.chomp
+
+      elsif answer == 1
+        puts "Type again the name"
+        name = gets.chomp
+
+        else
+        puts "All good then!"
+      end
+
 
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: country, height: height}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     #get another name from user
-    name = gets.chomp
-    hobbies = gets.chomp
-    country = gets.chomp
-    height = gets.chomp
+    name = gets.chomp.to_s
+    cohort = gets.chomp.to_s
+    # hobbies = gets.chomp
+    # country = gets.chomp
+    # height = gets.chomp
 
+    # so it loops for each student
+
+    if name.nil?
+      name = "Default"
+    end
+    if cohort.nil?
+      cohort = "november"
+    end
+
+    if !name.empty?
+      puts "Enter 1 to change the name, 2 to change the cohort or 3 to continue"
+      answer = gets.chomp.to_i
+      if  answer == 2
+        puts "Type the cohort again"
+        cohort = gets.chomp
+
+      elsif answer == 1
+        puts "Type again the name"
+        name = gets.chomp
+
+        else
+        puts "All good then!"
+      end
+    end
   end
+
 
 #return the array of students
 students
@@ -60,6 +113,7 @@ end
 
 
 def print_footer(students)
+  puts ""
   puts "Overall, we have #{students.count} great students"
 
 end
